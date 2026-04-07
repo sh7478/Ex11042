af35f5504f1fce15fa219d7c38900ac94950f602
sh7478 <sh7478@bs.amalnet.k12.il>
2026-04-07 12:29:45 +0300

    added helper class for the table sql database

---
+++ app/src/main/java/com/example/ex11042/Expenses.java
@@ -0,0 +1,10 @@
+package com.example.ex11042;
+
+public class Expenses {
+    public static final String TABLE_NAME = "Expenses";
+    public static final String KEY_ID = "Id";
+    public static final String DESCRIPTION = "Description";
+    public static final String AMOUNT = "Amount";
+    public static final String CATEGORY = "Category";
+    public static final String EXPENSE_TIME = "Expense_Time";
+}
